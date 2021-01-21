2f91c107688cc05b9da8705eb29cd67050cfb5f3
B-18-11-04 <72132726+B-18-11-04@users.noreply.github.com>
2021-01-21 23:45:47 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectmainvoid;
+
+/**
+ *
+ * @author bhasa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Void belajar = new Void();
+        belajar.KelompokHewan();
+        belajar.Perulangan();
+        belajar.perulangan2();
+    }
+}
